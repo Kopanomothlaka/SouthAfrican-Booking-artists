31cbd3fce748f00ab7de10ee35186808791d1848
kopanomothlaka <kopanomothlaka@gmail.com>
2025-07-16 00:38:51 +0200

    updated

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://sabookartists.netlify.app',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
